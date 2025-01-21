47344984fd9f0b8e96f93784984dbe640f932d1d
lukasdenfete <153369307+lukasdenfete@users.noreply.github.com>
2025-01-21 12:18:41 -0600

    Manual update

--- WhiskeySour.Web/Controllers/ProductController.cs
+++ WhiskeySour.Web/Controllers/ProductController.cs
@@ -18,5 +18,23 @@ public class ProductController : Controller
     {
         return View(await _context.Products.ToListAsync());
     }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
     
 }
