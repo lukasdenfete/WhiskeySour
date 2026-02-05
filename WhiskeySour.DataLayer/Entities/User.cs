79c394953ed04dc55edd7bf706523afb751ff378
lukasdenfete <153369307+lukasdenfete@users.noreply.github.com>
2026-02-05 16:01:36 +0100

    snyggat till startsidan

--- WhiskeySour.DataLayer/Entities/User.cs
+++ WhiskeySour.DataLayer/Entities/User.cs
@@ -6,6 +6,7 @@ public class User : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public byte[]? ProfilePicture { get; set; }
+    public DateTime JoinedDate { get; set; } =  DateTime.Now;
     public List<Order> Orders { get; set; } = new List<Order>();
     
 }
